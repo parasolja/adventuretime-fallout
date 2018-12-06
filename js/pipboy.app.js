a94daab0279b4c45c7456fe9c7b2bf830fa21511
parasolja <parasolja@gmail.com>
2018-12-06 22:06:37 +0100

    added images of the weapons to inventory page, synchronized weapon-name column with image column and info column

--- js/pipboy.app.js
+++ js/pipboy.app.js
@@ -4,51 +4,76 @@ $(document).ready(function(){
 	var weapons = [
 		{
 			"name": "scarlet",
-			"damage": medium,
-			"type": sword,
-			"owner": Finn
+			"damage": "medium",
+			"type": "sword",
+			"owner": "Finn",
+			"image": "scarlet.png"
 		},
 		{
 			"name": "root",
-			"damage": medium,
-			"type": sword,
-			"owner": Finn
+			"damage": "medium",
+			"type": "sword",
+			"owner": "Finn",
+			"image": "root.png"
+
 		},
 		{
 			"name": "finnsw",
-			"damage": heavy,
-			"type": sword,
-			"owner": Finn
+			"damage": "heavy",
+			"type": "sword",
+			"owner": "Finn",
+			"image": "finnsword.png"
+
 		},
 		{
-			"name": "night",
-			"damage": heavy,
-			"type": sword,
-			"owner": Finn
+			"name": "demon",
+			"damage": "heavy",
+			"type": "sword",
+			"owner": "Finn",
+			"image": "demon.png"
+
 		},
 		{
 			"name": "jake",
-			"damage": medium,
-			"type": sword,
-			"owner": none
+			"damage": "medium",
+			"type": "sword",
+			"owner": "none",
+			"image": "jake.png"
 		},
 		{
 			"name": "axbass",
-			"damage": medium,
-			"type": hybrid,
-			"owner": Marceline
+			"damage": "medium",
+			"type": "hybrid",
+			"owner": "Marceline",
+			"image": "axbass.png"
 		},
 		{
-			"name": "electode",
-			"damage": medium,
-			"type": weapon,
-			"owner": PrincessBubblegum
+			"name": "fiona",
+			"damage": "medium",
+			"type": "sword",
+			"owner": "Fiona",
+			"image": "fiona.png"
+			
 		}
 		];
 	
 	$('.item-list a').on('mouseenter', function(e){
 		var current_item = $(e.currentTarget).attr('class');
 		console.log(current_item);
+		
+		for(item in weapons){
+			if(weapons[item].name == current_item){
+				console.log(weapons[item])
+				
+				var container = $('.item-stats');
+				container.find('.damage').html(weapons[item].damage);
+				container.find('.type').html(weapons[item].type);
+				container.find('.owner').html(weapons[item].owner);
+				
+				var anotherContainer = $('.center-image-inv');
+				anotherContainer.children("img").attr("src", weapons[item].image);
+			}		
+		}
 	});
 	
 });
